d2acb82cb01684b1e59fd13f6f1b801fbc07cb03
didier01 <deramirez19@misena.edu.co>
2024-02-21 08:35:48 -0500

    cambio keys captcha

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,8 @@ export const environment = {
    url: 'https://honesolutionsbackproduction.azurewebsites.net/api/',
    PASS_ENC: '123456',
 
-   PUBLIC_PASS_KEY:'6LeAFnopAAAAAGjvMHJ9p-enzgSdq9yncRUqPeqK',
-   PRIVATE_PASS_KEY:'6LeAFnopAAAAAM4dPC7emi-kBVGFtXGlbPlYV_Dw'
+   PUBLIC_PASS_KEY:'6LfAw3opAAAAAJc3UghB3C04qOh2MeFC3WOKWOOK',
+   PRIVATE_PASS_KEY:'6LfAw3opAAAAAGPWt1-FzH_L0c3d-zEHjiMRoo7T'
+   // PUBLIC_PASS_KEY:'6LeAFnopAAAAAGjvMHJ9p-enzgSdq9yncRUqPeqK', viejas
+   // PRIVATE_PASS_KEY:'6LeAFnopAAAAAM4dPC7emi-kBVGFtXGlbPlYV_Dw'
 };
