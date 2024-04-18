e75169827a00226113d5ccfe7e429617a6083cae
fauner ramirez <faunerramirez@outlook.com>
2024-04-18 12:01:00 -0500

    se documenta funciones en nuevo modal de contactos del prestador, se eliminan comentarios en html en list-docuemnts

--- src/app/shared/modals/contacts-provider/contacts-provider.component.ts
+++ src/app/shared/modals/contacts-provider/contacts-provider.component.ts
@@ -22,8 +22,6 @@ export class ContactsProviderComponent implements OnInit {
   contacts: any = [];
   clientSelected: any = this.eventManager.clientSelected();
 
-  idProvider: any = 0;
-
   readonly #modal = inject(NzModalRef);
   readonly nzModalData: any = inject(NZ_MODAL_DATA);
 
@@ -40,11 +38,12 @@ export class ContactsProviderComponent implements OnInit {
 
 
   ngOnInit(): void {
-    this.idProvider = this.clientSelected.idProvider;
-
-    this.getContactsByIDProvider(this.idProvider);
+    this.getContactsByIDProvider(this.clientSelected.idProvider);
   }
 
+  /**
+   * contactsProvider: Formualario de contacto
+   */
   createFormContactProvider() {
     this.contactsProvider = this.formBuilder.group({
       idProvider: [''],
@@ -57,7 +56,9 @@ export class ContactsProviderComponent implements OnInit {
 
     });
   }
-
+  /**
+     * getContactsByIDProvider, función que consume servicios de todos los contactos por el prestador logueado 
+     */
   getContactsByIDProvider(idProvider: any) {
     this.contact.getContactById(idProvider).subscribe((data: any) => {
       this.contacts = data.contacts;
@@ -71,7 +72,9 @@ export class ContactsProviderComponent implements OnInit {
     });
   }
 
-
+  /**
+     * submitContactsProvider, función que valida ya sea para guardar o actualizar contacto y la llamamos en el html
+     */
   submitContactsProvider() {
     if (this.contactsProvider.invalid) {
       Object.values(this.contactsProvider.controls).forEach(control => {
@@ -89,22 +92,28 @@ export class ContactsProviderComponent implements OnInit {
     const payload: any = {
       ...docForm,
       idOccupation: 15,
-      idProvider: `${this.idProvider}`,  
+      idProvider: `${this.clientSelected.idProvider}`,
     };
-    if (payload.idContactsProvider) {      
+    if (payload.idContactsProvider) {
       this.putContact(payload);
 
-    } else {      
+    } else {
       const { idContactsProvider, ...contact } = payload;
       this.postContact(contact);
     }
 
   }
 
+  /**
+    * createNotificacion, función que podemos reutulizar para mostrar mensajes tanto en guardar y actualizar contacto
+    */
   createNotificacion(type: string, title: string, message: string) {
     this.notificationService.create(type, title, message);
   }
 
+  /**
+   * postContact, función que consume api para guardar contactos para el prestador que esta logueado
+   */
   postContact(payload: any) {
     this.contact.addContactsProvider(payload).subscribe({
       next: (res: any) => {
@@ -120,10 +129,11 @@ export class ContactsProviderComponent implements OnInit {
     });
   }
 
-
+  /**
+    * putContact, función que consume api para actulizar contactos para el prestador que esta logueado
+    */
   putContact(payload: any) {
-
-    this.contact.updateContactProvider(payload).subscribe({     
+    this.contact.updateContactProvider(payload).subscribe({
       next: (res: any) => {
         this.loader = false;
         this.createNotificacion('success', 'Representante legal Actualizado', 'El representante legal se actualizo correctamente.');
