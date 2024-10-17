5373f4d15e2cc0d37cb4abb30d10bf70eb2597fd
davidfernandoza <fernando.zapata.live@gmail.com>
2024-10-17 16:39:08 -0500

    Add files in production

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,12 +1,9 @@
 export const environment = {
-   production: true,
-   // url: 'https://backpruebashone.azurewebsites.net/api/',
-   // urlNewBack: 'http://localhost:8080/',
-   // url: 'http://localhost:8080/api/', //Para conectarse al back viejo
-   url: 'https://honesolutionsbackproduction.azurewebsites.net/api/',
-   urlNewBack: 'https://honesolutionsbacknew.azurewebsites.net/',
-   PASS_ENC: '123456',
+	production: true,
+	url: 'http://44.204.145.224:8080/api/',
+	urlNewBack: 'http://44.204.145.224:8081/',
 
-   PUBLIC_PASS_KEY:'6Ld-8nopAAAAAIw0RV8-HYPQdgzUdjeYn7gvIApc',
-   PRIVATE_PASS_KEY:'6Ld-8nopAAAAAHww4ho9iw_xAjKPxTabuKENAHDd'
+	PASS_ENC: '123456',
+	PUBLIC_PASS_KEY:'6Ld-8nopAAAAAIw0RV8-HYPQdgzUdjeYn7gvIApc',
+	PRIVATE_PASS_KEY:'6Ld-8nopAAAAAHww4ho9iw_xAjKPxTabuKENAHDd'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,8 @@
 export const environment = {
-   production: false,
-   // urlNewBack: 'http://localhost:8080/',//Para conectarse al back nuevo
-   // url: 'http://localhost:8080/api/', //Para conectarse al back viejo
-   // url: 'https://backpruebashone.azurewebsites.net/api/', //Para conectarse a pruebas
-   url: 'https://honesolutionsbackproduction.azurewebsites.net/api/',
-   urlNewBack: 'https://honesolutionsbacknew.azurewebsites.net/',
-   PASS_ENC: '123456',
-
-   PUBLIC_PASS_KEY:'6Ld-8nopAAAAAIw0RV8-HYPQdgzUdjeYn7gvIApc',
-   PRIVATE_PASS_KEY:'6Ld-8nopAAAAAHww4ho9iw_xAjKPxTabuKENAHDd'
+	production: false,
+	url: 'http://hone_solutions_api/api/',
+	urlNewBack: 'http://hone_solutions_back_new/',
+	PASS_ENC: '123456',
+	PUBLIC_PASS_KEY:'6Ld-8nopAAAAAIw0RV8-HYPQdgzUdjeYn7gvIApc',
+	PRIVATE_PASS_KEY:'6Ld-8nopAAAAAHww4ho9iw_xAjKPxTabuKENAHDd'
 };
