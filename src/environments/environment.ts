b430558b5e04bb5b80c2db0707f3416478443619
davidfernandoza <fernando.zapata.live@gmail.com>
2024-10-21 08:11:40 -0500

    add files in production

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
 	production: false,
-	url: 'http://hone_solutions_api/api/',
-	urlNewBack: 'http://hone_solutions_back_new/',
+	url: 'http://localhost:7000/api/',
+   urlNewBack: 'http://localhost:7001/',
 	PASS_ENC: '123456',
 	PUBLIC_PASS_KEY:'6Ld-8nopAAAAAIw0RV8-HYPQdgzUdjeYn7gvIApc',
 	PRIVATE_PASS_KEY:'6Ld-8nopAAAAAHww4ho9iw_xAjKPxTabuKENAHDd'
