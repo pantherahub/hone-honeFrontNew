8211f6470de43dbcc180eb45485c559e76e4e6aa
davidfernandoza <fernando.zapata.live@gmail.com>
2024-10-22 13:55:38 -0500

    Add files in production

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
-   production: false,
-   url: 'http://localhost:7000/api/',
-   urlNewBack: 'http://localhost:7001/',
+   production: true,
+	url: 'http://44.204.145.224:8080/api/',
+	urlNewBack: 'http://44.204.145.224:8081/',
    PASS_ENC: '123456',
    PUBLIC_PASS_KEY:'6Ld-8nopAAAAAIw0RV8-HYPQdgzUdjeYn7gvIApc',
    PRIVATE_PASS_KEY:'6Ld-8nopAAAAAHww4ho9iw_xAjKPxTabuKENAHDd'
