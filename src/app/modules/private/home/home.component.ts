3370d2e20a7a282df3f36f7979c0fb1f4c4a4a44
felipevcp <felipe.villamizar@pantherahub.com>
2025-04-07 13:18:16 -0500

    Hide mundial option in home

--- src/app/modules/private/home/home.component.ts
+++ src/app/modules/private/home/home.component.ts
@@ -70,7 +70,7 @@ export class HomeComponent implements OnInit, AfterViewInit, OnDestroy {
     this.loadingData = true;
     this.clientService.getClientListByProviderId(this.user.id).subscribe({
       next: (res: any) => {
-        this.clientList = res;
+        this.clientList = res.filter((client: any) => client.idClientHoneSolutions !== 10);
         this.applyFilter();
         this.loadingData = false;
       },
