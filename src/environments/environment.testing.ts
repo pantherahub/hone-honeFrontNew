dace6ba4e8ea6b15863309cb4871eeae06849a2a
felipevcp <felipe.villamizar@pantherahub.com>
2025-08-29 18:00:18 -0500

    Video tutorial and environments

--- src/environments/environment.testing.ts
+++ src/environments/environment.testing.ts
@@ -1,6 +1,10 @@
 export const environment = {
 	production: true,
 	url: 'http://98.85.0.61:8080/api/',
-	urlNewBack: 'http://98.85.0.61:8081/',
-	PUBLIC_PASS_KEY: '6LcojGQqAAAAADPhwhfQj7iHK9RZBcvCUKEf_s88',
+  urlNewBack: 'http://98.85.0.61:8081/',
+
+  PUBLIC_PASS_KEY: '6LcojGQqAAAAADPhwhfQj7iHK9RZBcvCUKEf_s88',
+
+  s3AssetsHost: 'https://honesolutiospublic.s3.us-east-1.amazonaws.com/',
+  azureAssetsHost: 'https://honesolutions.blob.core.windows.net/'
 };
