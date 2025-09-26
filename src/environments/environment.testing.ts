407dcd82a8c6faff61af660247316beadc97b4c2
felipevcp <felipe.villamizar@pantherahub.com>
2025-09-26 11:12:33 -0500

    Update assets bucket

--- src/environments/environment.testing.ts
+++ src/environments/environment.testing.ts
@@ -6,5 +6,4 @@ export const environment = {
   PUBLIC_PASS_KEY: '6LcojGQqAAAAADPhwhfQj7iHK9RZBcvCUKEf_s88',
 
   s3AssetsHost: 'https://honesolutiospublic.s3.us-east-1.amazonaws.com/',
-  azureAssetsHost: 'https://honesolutions.blob.core.windows.net/',
 };
