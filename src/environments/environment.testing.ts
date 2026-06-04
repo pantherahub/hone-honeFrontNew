4856b289f9424e36e711ab3b86044fd9c575c84c
felipevcp <felipe.villamizar@pantherahub.com>
2026-06-04 09:56:52 -0500

    Update testing api key

--- src/environments/environment.testing.ts
+++ src/environments/environment.testing.ts
@@ -2,7 +2,7 @@ export const environment = {
 	production: true,
 	url: 'http://98.85.0.61:8080/api/',
 
-  API_KEY: 'a1d6743f19b4d2b0e6d4b075d79a698a0830c4178263170ee3e40193eafe0940',
+  API_KEY: '4fe4ac134102fe710b157361e4d0b303ee5bf1e9bf7373077aba47e6e011eefd',
   PUBLIC_PASS_KEY: '6LcojGQqAAAAADPhwhfQj7iHK9RZBcvCUKEf_s88',
 
   s3AssetsHost: 'https://honesolutiospublic.s3.us-east-1.amazonaws.com/',
